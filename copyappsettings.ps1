ad2da02b08f91af83a387c559c4b61688033416f
rafael brandão <ratosoft@hotmail.com>
2018-10-22 12:52:07 -0300

    Ajuste no script original

---
+++ copyappsettings.ps1
@@ -0,0 +1,70 @@
+#PREENCHER AS VARIÁVEIS ABAIXO
+$subscriptionIdSource = ""
+$subscriptionIdTarget = ""
+
+$resourceGroupSource = ""
+$resourceGroupTarget = ""
+
+$webAppsource = ""
+$webAppTarget = ""
+
+#CASO NÃO UTILIZE SLOT, DEIXAR A VARIÁVEIS REFERENTE AO SLOT EM BRANCO
+$slotSource = ""
+$slotTarget = ""
+#PREENCHER AS VARIÁVEIS ACIMA
+
+Login-AzureRmAccount
+
+Set-AzureRmContext -subscriptionId $subscriptionIdSource
+
+# Load Existing Web App settings for source and target
+If($slotSource -eq "") {    
+    $webAppSource = Get-AzureRmWebApp -ResourceGroupName $resourceGroupSource -Name $webAppsource
+}
+Else {
+    $webAppSource = Get-AzureRmWebAppSlot -ResourceGroupName $resourceGroupSource -Name $webAppsource -Slot $slotSource
+}
+
+# Get reference to the source Connection Strings
+#$connectionStringsSource = $webAppSource.SiteConfig.ConnectionStrings
+
+
+# Create Hash variable for Connection Strings
+#$connectionStringsTarget = @{}
+
+# Copy over all Existing Connection Strings to the Hash
+#ForEach ($connStringSource in $connectionStringsSource) {
+    #$connectionStringsTarget[$connStringSource.Name] = @{ Type = $connStringSource.Type.ToString(); Value = $connStringSource.ConnectionString }
+#}
+    
+
+# Save Connection Strings to Target
+#If($slotTarget -eq "") {
+    #Set-AzureRmWebApp -ResourceGroupName $resourceGroupTarget -Name $webAppTarget ConnectionStrings $connectionStringsTarget
+#}
+#Else {
+    #Set-AzureRmWebAppSlot -ResourceGroupName $resourceGroupTarget -Name $webAppTarget -Slot $slotTarget -ConnectionStrings $connectionStringsTarget
+#}
+
+# Get reference to the source app settings
+$appSettingsSource = $webAppSource.SiteConfig.AppSettings
+
+
+Set-AzureRmContext -SubscriptionId $subscriptionIdTarget
+
+# Create Hash variable for App Settings
+$appSettingsTarget = @{}
+
+
+# Copy over all Existing App Settings to the Hash
+ForEach ($appSettingSource in $appSettingsSource) {
+    $appSettingsTarget[$appSettingSource.Name] = $appSettingSource.Value
+}
+
+# Save Connection Strings to Target
+If($slotTarget -eq "") {
+    Set-AzureRmWebApp -ResourceGroupName $resourceGroupTarget -Name $webAppTarget -AppSettings $appSettingsTarget
+}
+Else {
+    Set-AzureRmWebAppSlot -ResourceGroupName $resourceGroupTarget -Name $webAppTarget -Slot $slotTarget -AppSettings $appSettingsTarget
+}
